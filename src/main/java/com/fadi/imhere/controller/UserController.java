811a0f52b655bf3fea4ebba290b95e4bab6e83e3
Fadi NOUFAL <noufal4me@gmail.Com>
2018-11-22 13:25:43 +0100

    Localhost:4200 authorized

--- src/main/java/com/fadi/imhere/controller/UserController.java
+++ src/main/java/com/fadi/imhere/controller/UserController.java
@@ -15,6 +15,7 @@ import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping
 public class UserController {
