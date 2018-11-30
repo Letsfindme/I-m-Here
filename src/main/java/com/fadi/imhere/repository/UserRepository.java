7680b389c4c09fb3762afb9b1607370b1d9f6e56
Fadi NOUFAL <noufal4me@gmail.Com>
2018-11-30 17:04:25 +0100

    removed @repository

--- src/main/java/com/fadi/imhere/repository/UserRepository.java
+++ src/main/java/com/fadi/imhere/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.fadi.imhere.repository;
 
 import com.fadi.imhere.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("userRepository")
 public interface UserRepository extends JpaRepository<User, Long> {
-    User getUserByUsername(String username);
+   // User getUserByUsername(String username);
+    User findByEmail(String email);
 }
